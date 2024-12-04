4c584b7ab6c4d79e262c070c36e4ca48c4122ae6
lher-emilie <emilie.lher@etudiant.univ-brest.fr>
2024-12-04 11:23:53 +0100

    second titre

--- src/main/kotlin/vglobe/view.kt
+++ src/main/kotlin/vglobe/view.kt
@@ -1,6 +1,7 @@
 package vglobe
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +21,52 @@ import androidx.compose.ui.layout.ContentScale
 @Composable
 fun HeaderWithImage() {
     val image: ImageBitmap = useResource("images/departVG.png") { loadImageBitmap(it) }
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .aspectRatio(image.width.toFloat() / image.height.toFloat())
-    ) {
+   Column(
+       modifier = Modifier.fillMaxSize()
+   ) {
+       Box(
+           modifier = Modifier
+               .fillMaxWidth()
+               .aspectRatio(image.width.toFloat() / image.height.toFloat())
+       ) {
 
-        Image(
-            bitmap = image,
-            contentDescription = "Header Image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxWidth()
-                .height(300.dp)
-        )
-        Text(
-            text = "Vendée Globe Classement",
-            color = Color.White,
-            fontSize = 20.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(top = 16.dp)
-                .align(Alignment.Center)
-        )
-    }
+           Image(
+               bitmap = image,
+               contentDescription = "Header Image",
+               contentScale = ContentScale.Crop,
+               modifier = Modifier.fillMaxWidth()
+                   .height(300.dp)
+           )
+           Text(
+               text = "Vendée Globe Classement",
+               color = Color.White,
+               fontSize = 20.sp,
+               textAlign = TextAlign.Center,
+               modifier = Modifier
+                   .padding(top = 16.dp)
+                   .align(Alignment.Center)
+           )
+       }
 
+       Spacer(modifier = Modifier.height(16.dp)) // Espacement entre les blocs
+
+       // Section grisée contenant le titre
+       Box(
+           modifier = Modifier
+               .fillMaxWidth()
+               .background(Color.LightGray)
+               .padding(16.dp) // Padding interne de la Box
+       ) {
+           Column(modifier = Modifier.fillMaxWidth()) {
+               Text(
+                   text = "Classement Vendée Globe 2024",
+                   color = Color.Black,
+                   fontSize = 20.sp,
+                   fontWeight = FontWeight.Bold,
+                   textAlign = TextAlign.Center,
+                   modifier = Modifier.fillMaxWidth() // Centré horizontalement
+               )
+           }
+       }
+   }
 }
