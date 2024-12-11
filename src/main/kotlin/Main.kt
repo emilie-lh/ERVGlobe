0c7d8f48f6b2f3a931097d384b8b2a2a70442314
Romain22220 <79304081+iNaXozZ@users.noreply.github.com>
2024-12-11 17:37:52 +0100

    Ajout dossier dynamique + suppression import inutiles

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,10 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import vglobe.HeaderWithImage
-import vglobe.afficherClassement
 import vglobe.extraireDonnees
 
 @Composable
